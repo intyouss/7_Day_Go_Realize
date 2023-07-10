1f823dee1156f57898bb5ac04f47394894d44ac2
intyouss <123916296+intyouss@users.noreply.github.com>
2023-07-10 16:15:48 +0800

    add: Complete the initial frame

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,8 @@
 package gee
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type HandlerFunc func(w http.ResponseWriter, r *http.Request)
 
@@ -8,14 +10,27 @@ type Engine struct {
 	router map[string]HandlerFunc
 }
 
-func (e *Engine) ServeHttp() {}
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
 
-func (e *Engine) Run(port string) {
+func (e *Engine) addRoute(method, path string, handler HandlerFunc) {
+	key := method + "-" + path
+	e.router[key] = handler
+}
 
+func (e *Engine) Run(port string) (err error) {
+	err = http.ListenAndServe(port, e)
+	return
 }
 
 func (e *Engine) GET(path string, handler HandlerFunc) {
-
+	e.addRoute("GET", path, handler)
 }
 
 func New() *Engine {
