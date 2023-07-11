f12d376829718b9ed5c5b481698ea97632627715
intyouss <123916296+intyouss@users.noreply.github.com>
2023-07-11 11:37:02 +0800

    add: Add route resolution

--- gee/router.go
+++ gee/router.go
@@ -1,20 +1,69 @@
 package gee
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type router struct {
+	roots    map[string]*node
 	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
 	return &router{
+		roots:    make(map[string]*node),
 		handlers: make(map[string]HandlerFunc),
 	}
 }
 
-func (r *router) addRouter(method, path string, handler HandlerFunc) {
-	key := method + "-" + path
+func ParsePattern(pattern string) []string {
+	r := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range r {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router) addRouter(method, pattern string, handler HandlerFunc) {
+	parts := ParsePattern(pattern)
+	key := method + "-" + pattern
+	root, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	root.insert(pattern, parts, 0)
 	r.handlers[key] = handler
+}
+
+func (r *router) getRouter(method string, pattern string) (*node, map[string]string) {
+	searchParts := ParsePattern(pattern)
+	params := make(map[string]string, 0)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	node := root.search(searchParts, 0)
+	if node != nil {
+		parts := ParsePattern(node.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(parts) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return node, params
+	}
+	return nil, nil
 }
 
 func (r *router) handler(c *Context) {
