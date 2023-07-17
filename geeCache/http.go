27faebe891abbaaf00b779869b74c73089e49df8
intyouss <123916296+intyouss@users.noreply.github.com>
2023-07-17 12:58:24 +0800

    add&fix: distributed node and update HTTPPool

--- geeCache/http.go
+++ geeCache/http.go
@@ -2,16 +2,26 @@ package geeCache
 
 import (
 	"fmt"
+	"geeCache/consistenthash"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultPath = "/geeCache/"
+const (
+	defaultPath     = "/geeCache/"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	selfAddr string
-	basePath string
+	selfAddr    string
+	basePath    string
+	mutex       sync.Mutex
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter
 }
 
 func NewHTTPPool(selfAddr string) *HTTPPool {
@@ -51,4 +61,55 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.b)
+}
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{
+			baseURL: peer + p.basePath,
+		}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.selfAddr {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _PeerGetter = (*HTTPPool)(nil)
+
+type httpGetter struct {
+	baseURL string
+}
+
+func (h *httpGetter) Get(group, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
 }
